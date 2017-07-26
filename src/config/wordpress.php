42b11355e9a12b34e931c79ba6b974d2d0184fc3
林振宇 <admin@drakframe.com>
2017-07-26 15:17:08 +0800

    重新加入wordpress配置文件

---
+++ src/config/wordpress.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    // example: http://www.wordpress.dev/wp-json/
+    // if you don't enable pretty permalinks, you need to fill http://www.wordpress.dev/?rest_route=
+    'endpoint' => 'http://www.wordpress.dev/wp-json/',
+    
+    // your wordpress account
+    'account' => [
+        'username' => '',
+        'password' => '',
+    ]
+];
